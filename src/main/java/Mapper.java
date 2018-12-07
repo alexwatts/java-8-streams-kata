5656ca1a586674267594cb0b719683c8053507bd
Alex Watts <alex_watts@talk21.com>
2018-12-07 16:48:00 +0000

    finished

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -1,13 +1,13 @@
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Mapper {
 
     public List<Orange> mapApplesToOranges(List<Apple> apples) {
-
-        return apples.stream().map(apple -> new Orange()).collect(Collectors.toList());
-        
+        //apples.stream().map(...)
+        return Collections.EMPTY_LIST;
     }
     
 }
