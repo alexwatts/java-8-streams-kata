a1f7256d9ac02b902b10eb4f6840f81213c380f4
Alex Watts <alex_watts@talk21.com>
2018-12-07 15:31:05 +0000

    Fixed compilation

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -3,9 +3,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Mapper {
-    
-        //apples.stream().map(...)
+
+    public List<Orange> mapApplesToOranges(List<Apple> apples) {
+
         return apples.stream().map(apple -> new Orange()).collect(Collectors.toList());
+        
     }
     
 }
