3e61a8baf3ab8157d444753d0cf5ccd9ffeb5a0d
Alex Watts <alex_watts@talk21.com>
2018-12-07 14:11:26 +0000

    map-try: map try

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -1,10 +1,11 @@
-import java.util.Collections;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Mapper {
     
-    public List<Orange> mapApplesToOranges(List<Apple> apple) {
-        return Collections.emptyList();
+    public List<Orange> mapApplesToOranges(List<Apple> apples) {
+        return apples.stream().map(apple -> new Orange()).collect(Collectors.toList());
     }
     
 }
