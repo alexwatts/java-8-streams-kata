9e3037b1f5ef3015885cc7607f59a27b4187a0da
Alex Watts <alex_watts@talk21.com>
2018-12-07 14:01:21 +0000

    Iniital commit

---
+++ src/main/java/Mapper.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Mapper {
+    
+    public List<Orange> mapApplesToOranges(List<Apple> apple) {
+        return Collections.emptyList();
+    }
+    
+}
---
+++ src/test/java/MapTest.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class MapTest {
+    
+    Mapper mapper = new Mapper();
+    
+    @Test
+    public void shouldMapApplesToOranges() {
+        
+        List<Apple> apples = givenApples();
+        List<Orange> mappedList = mapper.mapApplesToOranges(apples);
+        assertThat(mappedList.size(), equalTo(3));
+        
+    }
+       
+    private List<Apple> givenApples() {
+        return Arrays.asList(new Apple(), new Apple(), new Apple());
+    }
+}
