4958a466a94b8e52cf275b5f42af5136f760bbdc
Alex Watts <alex_watts@talk21.com>
2018-12-10 13:23:23 +0000

    test-pr: sdfsdf

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -1,4 +1,3 @@
-
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,7 +6,7 @@ public class Mapper {
 
     public List<Orange> mapApplesToOranges(List<Apple> apples) {
         //apples.stream().map(...)
-        return Collections.EMPTY_LIST;
+        return apples.stream().map(apple -> new Orange()).collect(Collectors.toList());
     }
     
 }
